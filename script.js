defeb6b3189ba9b50a1a5a493fb12a5fcffe2457
Noman Shahid <noman-shahid@hotmail.com>
2016-10-16 21:56:38 -0400

    Updated files for 2017 task

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-var maxWeight = 100 * 1000;
-var totalWeight = 0;
 var totalPrice = 0;
 var totalPriceWithTax = 0;
+var i = 1;
 
 $(document).ready(function () {
 	$('.info').hide();
-	$.getJSON("http://shopicruit.myshopify.com/products.json", function (obj) {
-		$.each(obj.products, function (key, val) {
-				if (this.product_type === "Keyboard" || this.product_type === "Computer") {
-					$.each(this.variants, function (key, val) {	
-						if (totalWeight + this.grams <= maxWeight) {
-							totalWeight += this.grams;
+	while (i < 15) {
+		$.getJSON("http://shopicruit.myshopify.com/products.json?page=" + i.toString(), function (obj) {
+				$.each(obj.products, function (key, val) {
+					if (this.product_type === "Clock" || this.product_type === "Watch") {
+						$.each(this.variants, function (key, val) {
 							totalPrice += Number(this.price);
-							if (this.taxable === true) {
+							if (this.taxable == true) {
 								totalPriceWithTax += Number(this.price) * 1.13; 
 							}
 							else { 
 								totalPriceWithTax += Number(this.price);
 							}
-						}
-					});
-				}
-			}); 
+						});
+					}
+				}); 
 		});
-	});
-	
+		++i;
+	}
+
+});
+
+
 $(document).one('click', '#button', function () {
-	$('#weight').append(totalWeight / 1000 + "kg");
-	$('#sub-total').append(totalPrice);
+	$('#sub-total').append(Math.floor(totalPrice*100)/100);
 	$('#total').append(Math.floor(totalPriceWithTax*100)/100);
 	$('.info').fadeIn(500);
-	});
+});
 
